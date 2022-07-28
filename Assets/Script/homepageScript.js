0fe1f0dab611dc963a2a3f9f205cab03ed2030bb
Mahmood <mahmoodhamid@hotmail.co.uk>
2022-07-28 18:00:52 +0100

    update to css, html and script, however an overall loss in function

--- Assets/Script/homepageScript.js
+++ Assets/Script/homepageScript.js
@@ -8,47 +8,14 @@ var highscores = document.getElementById("scorelink");
 var scoretracker = document.getElementById("scoredisplay")
 var question = document.getElementById("question");
 var options = document.querySelectorAll(".choice-option");
-var score = document.getElementById("score")
-var answer = document.getElementById("choice-box")
-var seconds=60
-var figure = "0"
-var questions1 = {
-    question: "console.log() is a method that prints meassages to where?",
-    choice1: "The Html",
-    choice2: "The Console",
-    choice3: "The Homepage screen",
-    choice4: "The root element",
-    answer: 2,
-}
-var questions2 = {
-    question: "Given 'students' is the name of a variable, how would you view its values datatype?",
-    choice1: "console.log(typeof students)",
-    choice2: "console.log(datatype students)",
-    choice3: "datatype.student;",
-    choice4: "typeof.students;",
-    answer: 1,
-}
-var questions3 = {
-    question: "To print the 3rd value of the array 'students' in the console, what would you use?",
-    choice1: "console.log(studesnts[3])",
-    choice2: "console.log(studesnts[2])",
-    choice3: "studesnts[2];",
-    choice4: "studesnts[3].value;",
-    answer: 2,
-} 
-var questions4 = {
-    question: "'document.querySelector(<p>)' would do which of the following?",
-    choice1: "Change the value of all p elements",
-    choice2: "Change the value of the first p element",
-    choice3: "Select all p elements in the body",
-    choice4: "Select the first p element in the body",
-    answer: 4,
-} 
+var score = document.getElementById("score");
+var answer = document.querySelectorAll(".choice-box");
+var seconds= 60;
+var figure = 0;
 
 
 function displayScore() {
-    score.textContent = figure
-
+    score.textContent = figure;
 }
 
 // Init function for page load up 
@@ -59,8 +26,8 @@ function init () {
     quizScreen.style.display = "none";
     saveScreen.style.display = "none";
     timer.style.display = "none";
-    scoretracker.style.display= "none"
-    figure = 0
+    scoretracker.style.display= "none";
+    figure = "0";
 }
 
 // Start button for the quiz to start 
@@ -86,12 +53,167 @@ quizStart.addEventListener("click", function(event){
          seconds--;
       } else {
          clearInterval(secDown);
+         displaySaveScreen();
       }
     },1000)
 
     addQuestion1();
 
 })
+
+
+// Adding questions function
+
+function addQuestion1 (){
+    
+    var questions1 = {
+        question: "console.log() is a method that prints meassages to where?",
+        choice1: "The Html",
+        choice2: "The Console",
+        choice3: "The Homepage screen",
+        choice4: "The root element",
+    }
+
+    question.textContent=questions1.question
+    options[0].textContent = questions1.choice1
+    options[1].textContent = questions1.choice2
+    options[2].textContent = questions1.choice3
+    options[3].textContent = questions1.choice4
+
+    for (var i = 0; i < answer.length; i ++ ){
+        
+        answer[i].addEventListener("click", function(event){
+          
+            var selected = event.target;
+            
+            var correct = options.getAttribute("data-answer=1")
+        
+
+            if (questions1.value) {
+                figure ++;
+                displayScore();
+                addQuestion2();
+                return;
+            } else {
+                seconds = seconds - 10;
+                addQuestion2();
+                return;
+            }
+        })
+    }    
+}
+
+function addQuestion2 (){
+
+
+    var questions2 = {
+        question: "Given 'students' is the name of a variable, how would you view its values datatype?",
+        choice1: "console.log(typeof students)",
+        choice2: "console.log(datatype students)",
+        choice3: "datatype.student;",
+        choice4: "typeof.students;",
+    }
+    
+    question.textContent=questions2.question
+    options[0].textContent = questions2.choice1
+    options[1].textContent = questions2.choice2
+    options[2].textContent = questions2.choice3
+    options[3].textContent = questions2.choice4
+
+    for (var i = 0; i < answer.length; i ++ ){
+
+        answer[i].addEventListener("click", function(event){
+
+            var selected = event.target;
+          
+            if (selected.dataset.answer == "0") {
+                figure ++;
+                displayScore();
+                addQuestion3();
+                return;
+            } else {
+                seconds = seconds - 10;
+                addQuestion3();
+                return;
+            }
+        })
+    }
+}
+
+function addQuestion3 (){
+
+    var questions3 = {
+        question: "To print the 3rd value of the array 'students' in the console, what would you use?",
+        choice1: "console.log(studesnts[3])",
+        choice2: "console.log(studesnts[2])",
+        choice3: "studesnts[2];",
+        choice4: "studesnts[3].value;",
+    } 
+
+    question.textContent=questions3.question
+    options[0].textContent = questions3.choice1
+    options[1].textContent = questions3.choice2
+    options[2].textContent = questions3.choice3
+    options[3].textContent = questions3.choice4
+
+    for (var i = 0; i < answer.length; i ++ ){
+
+        answer[i].addEventListener("click", function(event){
+          
+            var selected = event.target;
+
+
+            if (selected) {
+                figure ++;
+                displayScore();
+                addQuestion4();
+                return;
+            } else {
+                seconds = seconds - 10;
+                addQuestion4();
+                return;
+            }
+        })
+    }
+
+}
+
+function addQuestion4 (){
+
+    var questions4 = {
+        question: "'document.querySelector(<p>)' would do which of the following?",
+        choice1: "Change the value of all p elements",
+        choice2: "Change the value of the first p element",
+        choice3: "Select all p elements in the body",
+        choice4: "Select the first p element in the body",
+    } 
+    
+    question.textContent=questions4.question
+    options[0].textContent = questions4.choice1
+    options[1].textContent = questions4.choice2
+    options[2].textContent = questions4.choice3
+    options[3].textContent = questions4.choice4
+
+    for (var i = 0; i < answer.length; i ++ ){
+
+        answer[i].addEventListener("click", function(event){
+
+            var selected = event.target;
+          
+            if (selected.dataset.answer == "3") {
+                figure ++;
+                displayScore();
+                displaySaveScreen();
+                return;
+            } else {
+                seconds = seconds - 10;
+                displaySaveScreen();
+                return;
+            }
+        })
+    }
+
+}
 
 // Displays saving screen
 
@@ -102,120 +224,6 @@ function displaySaveScreen (){
     scoretracker.style.display= "none";
     highscores.style.display = "none";
     saveScreen.style.display = "block";
-}
-
-
-// Adding questions function
-
-function addQuestion1 (){
-
-    question.textContent=questions1.question
-    options[0].textContent = questions1.choice1
-    options[1].textContent = questions1.choice2
-    options[2].textContent = questions1.choice3
-    options[3].textContent = questions1.choice4
-
-    options.forEach(item => {
-        item.addEventListener('click', function(event){
-            var select = event.target
-            console.log(select.value)
-
-            if(select.matches("#choice-box") && dataset.answer === "2"){
-                figure = figure + 100;
-                displayScore();
-                addQuestion2();
-            }else {
-                seconds = seconds - 5;
-                displayScore();
-                addQuestion2();
-            }
-       
-              
-
-        })
-    })
-        
-}
-
-function addQuestion2 (){
-    question.textContent=questions2.question
-    options[0].textContent = questions2.choice1
-    options[1].textContent = questions2.choice2
-    options[2].textContent = questions2.choice3
-    options[3].textContent = questions2.choice4
-
-    options.forEach(item => {
-        item.addEventListener('click', function(event){
-            var select = event.target
-            console.log(select.value)
-
-            if(select.matches("#choice-box") && dataset.answer === "1"){
-                figure = figure + 100;
-                displayScore();
-                addQuestion3();
-            }else {
-                seconds = seconds - 5;
-                displayScore();
-                addQuestion3();
-             }
-
-        })
-    })
-
-}
-
-function addQuestion3 (){
-    question.textContent=questions3.question
-    options[0].textContent = questions3.choice1
-    options[1].textContent = questions3.choice2
-    options[2].textContent = questions3.choice3
-    options[3].textContent = questions3.choice4
-
-    options.forEach(item => {
-        item.addEventListener('click', function(event){
-            var select = event.target
-            console.log(select.value)
-
-            if(select.matches("#choice-box") && dataset.answer === "2"){
-                figure = figure + 100;
-                displayScore();
-                addQuestion4();
-            }else {
-                seconds = seconds - 5;
-                displayScore();
-                addQuestion4();
-            }
-
-        })
-    })
-
-}
-
-function addQuestion4 (){
-    question.textContent=questions4.question
-    options[0].textContent = questions4.choice1
-    options[1].textContent = questions4.choice2
-    options[2].textContent = questions4.choice3
-    options[3].textContent = questions4.choice4
-
-    options.forEach(item => {
-        item.addEventListener('click', function(event){
-            var select = event.target
-            console.log(select.value)
-
-            if(select.matches("#choice-box") && dataset.answer === "4"){
-                figure = figure + 100;
-                displayScore();
-                displaySaveScreen();
-            }else {
-                seconds = seconds - 5;
-                displayScore();
-                displaySaveScreen();
-            }
-
-        })
-    })
-
 }
 
 document.getElementById("start-again").addEventListener("click",function(){
---
+++ README.md
@@ -0,0 +1,56 @@
+# Javascript Quiz (Current Status: Not Dysfunctional)
+
+## Description
+
+This task has proven to have been more of a challenge than i initially anticipated. 
+It doesnt help that I started in the completely wrong direction in reguards to to html elements and storing my questions appropriately, with very little time to work on it.
+
+
+This is an object version of my questions which would help me go in the right direction. 
+I will have to  unfortunately redo this task and reformat all of my files. 
+
+
+// Questions
+var questions = [
+    {
+      title: "console.log() is a method that prints meassages to where?",
+      multiChoice: ["The Html", "The Console", "The Homepage screen", "The root element"],
+      answer: "The Console"
+    },
+  
+    {
+      title: "Given 'students' is the name of a variable, how would you view its values datatype?",
+      multiChoice: ["console.log(typeof students)", "console.log(datatype students)", "datatype.student;", "typeof.students;"],
+      answer: "console.log(typeof students)"
+    },
+  
+    {
+      title: "To print the 3rd value of the array 'students' in the console, what would you use?",
+      multiChoice: ["console.log(studesnts[3])", "console.log(studesnts[2])", "studesnts[2];", "studesnts[3].value;"],
+      answer: "console.log(studesnts[2])"
+    },
+  
+    {
+      title: "'document.querySelector(< p >)' would do which of the following?",
+      multiChoice: ["Change the value of all p elements", "Change the value of the first p element", "Select all p elements in the body", "Select the first p element in the body"],
+      answer: ".push()"
+    },
+  
+  ];
+
+## Screenshots
+
+![Screenshot 1](./Assets/Images/Screenshot%201.png)
+![Screenshot 2](./Assets/Images/Screenshot%202.png)
+
+These are two images of my homepage and scoreboard page, I was able to create functioning buttons which displayed the answers on the appropriate buttons, and lead to the next question as writen in the assessment criteria but they ceased to function as selecting an event listener with the format I have used caused some issues. Im not very pleased with the outcome but I have a better insight into appropriate ways to store information. 
+
+## Link to Deployed application (not functioning)
+
+
+
+
+
+
+
+
